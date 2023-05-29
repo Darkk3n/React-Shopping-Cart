5b7b4c43e1d5c08d9502f54e55317422645c98f0
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-29 13:21:48 -0600

    Implemented synchronization of the cart and UI

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,27 @@
+/* eslint-disable react/prop-types */
 import { useId } from "react";
+import { useCart } from "../hooks/useCart.js";
 import "./Cart.css";
 import { CartIcon, ClearCartIcon } from "./Icons.jsx";
 
 export function Cart() {
 	const cartCheckBoxId = useId();
+	const { cart, clearCart, addToCart } = useCart();
+
+	function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+		return (
+			<li>
+				<img src={thumbnail} alt={title} />
+				<div>
+					<strong>{title}</strong> - ${price}
+				</div>
+				<footer>
+					<small>Qty: {quantity}</small>
+					<button onClick={addToCart}>+</button>
+				</footer>
+			</li>
+		);
+	}
 	return (
 		<>
 			<label className="cart-button" htmlFor={cartCheckBoxId}>
@@ -12,21 +30,11 @@ export function Cart() {
 			<input id={cartCheckBoxId} type="checkbox" hidden />
 			<aside className="cart">
 				<ul>
-					<li>
-						<img
-							src="https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-							alt="iphone"
-						/>
-						<div>
-							<strong>iPhone</strong> - $1499
-						</div>
-						<footer>
-							<small>Qty:1</small>
-							<button>+</button>
-						</footer>
-					</li>
+					{cart.map((i) => (
+						<CartItem key={i.id} addToCart={() => addToCart(i)} {...i} />
+					))}
 				</ul>
-				<button>
+				<button onClick={clearCart}>
 					<ClearCartIcon />
 				</button>
 			</aside>
