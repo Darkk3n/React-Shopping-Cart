94564302349850474639752f77afde78b1753ef0
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-29 13:21:53 -0600

    Restored footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,10 @@
-import { useCart } from "../hooks/useCart";
 import "./Footer.css";
 
 export function Footer() {
-	// const { filters } = useFilters()
-	const { cart } = useCart();
-
 	return (
 		<footer className="footer">
-			{JSON.stringify(cart, null, 2)}
-			{/* <h4>Prueba técnica de React ⚛️</h4>
-			<h5>Shopping Cart con useContext & useReducer</h5> */}
+			<h4>Prueba técnica de React ⚛️</h4>
+			<h5>Shopping Cart con useContext & useReducer</h5>
 		</footer>
 	);
 }
