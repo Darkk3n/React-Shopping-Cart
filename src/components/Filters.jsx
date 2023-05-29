57499462233a49aa3470f821ff080d8563d24626
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-28 22:55:48 -0600

    useId implementation

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useId, useState } from "react";
 import "./Filters.css";
 
 export function Filters({ changeFilters }) {
 	const [minPrice, setMinPrice] = useState(0);
+	const minPriceFilterId = useId();
+	const categoryFilterId = useId();
 
 	const handleChangeMinPrice = (event) => {
 		setMinPrice(event.target.value);
@@ -22,10 +24,10 @@ export function Filters({ changeFilters }) {
 	return (
 		<section className="filters">
 			<div>
-				<label htmlFor="price">Starting Price</label>
+				<label htmlFor={minPriceFilterId}>Starting Price</label>
 				<input
 					type="range"
-					id="price"
+					id={minPriceFilterId}
 					min="0"
 					max="1000"
 					onChange={handleChangeMinPrice}
@@ -33,8 +35,8 @@ export function Filters({ changeFilters }) {
 				<span>${minPrice}</span>
 			</div>
 			<div>
-				<label htmlFor="category">Category</label>
-				<select id="category" onChange={handleChangeCategory}>
+				<label htmlFor={categoryFilterId}>Category</label>
+				<select id={categoryFilterId} onChange={handleChangeCategory}>
 					<option value="all">All</option>
 					<option value="laptops">Laptops</option>
 					<option value="smartphones">Smartphones</option>
