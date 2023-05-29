7a62cfabbdbab56f03ca6b4740f9e5d613bd246c
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-05-29 12:19:55 -0600

    Added comment

--- src/context/filters.jsx
+++ src/context/filters.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
 
-export const FiltersContext = createContext();
+export const FiltersContext = createContext(); //only created once
 
 export function FilterProvider({ children }) {
 	const [filters, setFilters] = useState({ category: "all", minPrice: 250 });
